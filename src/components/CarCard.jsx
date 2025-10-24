f027f3865a7693c2515e2764f91918485c9c6523
David <david.samah@student.moringaschool.com>
2025-10-24 12:02:41 +0300

    second edit

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 function CarCard({ car }) {
     return (
         <div className="car-card">
+            <h4>Our previous customer purchases(</h4>
             <h3>{car.model}</h3> {/* Use curly braces to dynamically display the model */}
             <p><strong>Color:</strong> {car.color}</p>
-            <p><strong>Year of Production:</strong> {car.year}</p> {/* Added colon after 'Year of Production' */}
+            <p><strong>Year of Production:</strong> {car.year}</p> 
         </div>
     );
 }
